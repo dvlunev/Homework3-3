1d6fb9220249b006973911eaa7fb491a37808118
dvlunev <dvlunev@ya.ru>
2023-01-15 12:04:00 +0300

    первый коммит

---
+++ src/main/java/me/lunev/homework33/Homework33Application.java
@@ -0,0 +1,13 @@
+package me.lunev.homework33;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework33Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework33Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/lunev/homework33/Homework33ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lunev.homework33;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework33ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
